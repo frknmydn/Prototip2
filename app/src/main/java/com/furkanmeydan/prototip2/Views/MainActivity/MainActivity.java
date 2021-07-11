16e835d06264b0c57fc2ba02fcdc0c3618a09bc2
furkan <furkan.meydan@hotmail.com>
2021-07-11 15:58:45 +0300

    kötü layout

--- app/src/main/java/com/furkanmeydan/prototip2/Views/MainActivity/MainActivity.java
+++ app/src/main/java/com/furkanmeydan/prototip2/Views/MainActivity/MainActivity.java
@@ -11,12 +11,14 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 //import com.furkanmeydan.prototip2.HomeFragmentDirections;
 import com.furkanmeydan.prototip2.DataLayer.LocalDataManager;
@@ -46,7 +48,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private FrameLayout mainFrameLayout;
     private ConstraintLayout constraintLayout;
     private Fragment fragment;
-    Button btnOpenDrawable;
+    ImageButton btnOpenDrawable;
 
     private String genderString, nameSurnameString, eMailString,profilePic,birthDate,oneSignalID;
 
